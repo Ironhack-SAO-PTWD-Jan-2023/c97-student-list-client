39b3702f9a9c417b2da245550db8770646c43ef8
D-Kunrath <dkunrath@gmail.com>
2023-03-29 21:33:08 -0300

    criado footer e ajustado tamanho do conteúdo da página

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="*" element={<div>404 - page not found</div>} />
-      </Routes>
+      <div className="page">
+        <Routes>
+          <Route path="*" element={<div>404 - page not found</div>} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
