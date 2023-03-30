08cbe8edd1af21002222fc8a7551cc3efeff39e3
D-Kunrath <dkunrath@gmail.com>
2023-03-29 21:01:26 -0300

    navbar feita sem estilização final

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom';
+import './style.css';
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="logo">
+        <NavLink to="/">
+          <h1>List de Alunos EX</h1>
+        </NavLink>
+      </div>
+      <div className="nav-actions">
+        <NavLink to="/alunos"><button>Consultar Alunos</button></NavLink>
+        <NavLink to="/alunos/novo"><button>Adicionar Aluno</button></NavLink>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
